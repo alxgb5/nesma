d437b565afbda99849b1917adc8193e445863af4
Alexandre BG <alexandre.brungiglio@gmail.com>
2023-02-18 02:29:19 +0100

    fix. move files

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { PrismaClientExceptionFilter } from './handlers/prisma-client-exception.filter';
+import { PrismaClientExceptionFilter } from './core/handlers/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
