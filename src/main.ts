3ecc5378eb9bcd83fbe641e1e6ebdfd70c0173c5
Alexandre BG <alexandre.brungiglio@gmail.com>
2023-02-18 01:16:00 +0100

    feat. intialize project

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from './handlers/prisma-client-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Prisma Boilerplate')
+    .setDescription('The NestJS Prisma Boilerplate API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  await app.listen(3000);
+}
+bootstrap();
